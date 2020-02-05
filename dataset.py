917eb2bc65245fe61a926c85400b1683eab429dd
domoniquecox <60198464+domoniquecox@users.noreply.github.com>
2020-02-05 14:01:23 -0600

    Update dataset.py

--- dataset.py
+++ dataset.py
@@ -35,7 +35,7 @@ while True:
     frame = imutils.resize(frame, width=400)
 
     #detect faces in the grayscale frame
-    rects = detector  .detectMultiScale(
+    rects = detector.detectMultiScale(
         cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY), scaleFactor=1.1,
         minNeighbors=5, minSize=(30,30)
     )
