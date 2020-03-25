1ffe51763c3cef0d72afeeca13281471945bdc9e
rjones102 <48767121+rjones102@users.noreply.github.com>
2020-03-25 17:22:04 -0500

    Update 03_face_recognition.py

--- OpenCV-Face-Recognition/FacialRecognition/03_face_recognition.py
+++ OpenCV-Face-Recognition/FacialRecognition/03_face_recognition.py
@@ -2,7 +2,9 @@
 import cv2
 import numpy as np
 import os
+from firebase import firebase
 
+FBConn = firebase.FirebaseApplication("https://eagle-eye-f3a0d.firebaseio.com/", None)
 
 recognizer = cv2.face.LBPHFaceRecognizer_create()
 recognizer.read('trainer/trainer.yml')
@@ -49,13 +51,26 @@ while True:
         # Check if confidence is less than 100 ==> "0" is perfect match 
         if (confidence < 100):
             id = names[id]
-            confidence = "  {0}%".format(round(100 - confidence))
+            confidence2 = "  {0}%".format(round(100 - confidence))
+            if (confidence > 95):
+                data_to_upload = {
+                    'Face Recognized' : id 
+                }
+
+                result = FBConn.post('/MyTestData/', data_to_upload)
+                
+
+                print (result)
+                break
+            
         else:
             id = "unknown"
             confidence = "  {0}%".format(round(100 - confidence))
         
         cv2.putText(img, str(id), (x+5,y-5), font, 1, (255,255,255), 2)
         cv2.putText(img, str(confidence), (x+5,y+h-5), font, 1, (255,255,0), 1)  
+
+
     
     cv2.imshow('camera',img) 
 
