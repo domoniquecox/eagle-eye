749b82a9af6de40a31beec28c1b171d2fcd0b397
rjones102 <48767121+rjones102@users.noreply.github.com>
2020-04-23 19:21:47 -0500

    Create auth.js

---
+++ static/js/auth.js
@@ -0,0 +1,67 @@
+/*const ui = new firebaseui.auth.AuthUI(firebase.auth());
+
+const uiConfig = {
+
+  callbacks: {
+    signInSuccessWithAuthResult(authResult, redirectUrl){
+        console.log("Success!");
+      return true;
+    },
+
+
+
+    firebase.auth().onAuthStateChanged(user => {
+  if(user) {
+    window.location = 'index.html'; //After successful login, user will be redirected to home.html
+}
+}
+
+    uiShown() {
+      document.getElementById('loader').style.display = 'none';
+    },
+  },
+  signInFlow: 'popup',
+  signInSuccessUrl: 'signedIn',
+  signInOptions: [
+    firebase.auth.EmailAuthProvider.PROVIDER_ID,
+    //firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+  ],
+};
+ui.start('#firebaseui-auth-container', uiConfig);
+
+*/
+
+
+
+
+const ui = new firebaseui.auth.AuthUI(firebase.auth());
+
+const uiConfig = {
+ callbacks: {
+   signInSuccessWithAuthResult(authResult, redirectUrl) {
+     return true;
+
+/*
+firebase.auth().onAuthStateChanged(function(user)){
+if(user) {
+window.location = 'courses.html'; //After successful login, user will be redirected to courses.html
+}
+}
+*/
+
+
+   },
+   uiShown() {
+     document.getElementById('loader').style.display = 'none';
+   },
+ },
+ signInFlow: 'popup',
+ //signInSuccessUrl: 'signedIn',
+ signInSuccessUrl: '../templates/courses.html',
+ signInOptions: [
+   firebase.auth.EmailAuthProvider.PROVIDER_ID,
+   //firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+ ],
+ //window.location.href = courses.html;
+};
+ui.start('#firebaseui-auth-container', uiConfig);
